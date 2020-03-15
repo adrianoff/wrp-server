aee110ec5b695b5415633ea1ce3d9f57dc0af38f
adrianoff <adrianov87@gmail.com>
2020-03-15 14:25:53 +0300

    parser

--- core/gallerix_parser.py
+++ core/gallerix_parser.py
@@ -1,5 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wrp_server.settings')
+
+import django
+django.setup()
+
+from core.models import Picture, Painter
+
+BASE_URL = 'https://gallerix.ru'
 
 painters_urls = [
     #'https://google.com'
@@ -7,9 +16,29 @@ painters_urls = [
 ]
 
 
-#headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+headers = {
+    'Host': 'gallerix.ru',
+    'Connection': 'keep-alive',
+    'DNT': '1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36',
+    'Sec-Fetch-User': '?1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-Mode': 'navigate',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,de;q=0.6,uk;q=0.5'
+}
 
 for painter_url in painters_urls:
-    req = requests.get(painter_url)
+    req = requests.get(painter_url, headers=headers, verify=False)
     soup = BeautifulSoup(req.content, 'html.parser')
-    links = soup.a['animsition-link']
+
+    painter_name = str(soup.find('b', itemprop='name').contents[0])
+    painter_object = Painter.objects.filter(url=painter_url)
+
+    divs = soup.find_all("div", class_="pic")
+
+    for div in divs[0:20]:
+        picture_link = BASE_URL + div.find('a')['href']
+
