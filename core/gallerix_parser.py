538117b7951ef0cd3091bb39df8ba93264d9ac50
adrianoff <adrianov87@gmail.com>
2020-03-17 23:34:22 +0300

    parser

--- core/gallerix_parser.py
+++ core/gallerix_parser.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import os
+import uuid
+
+from django.core.files import File
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wrp_server.settings')
 
 import django
@@ -54,8 +58,32 @@ for painter_url in painters_urls:
         try:
             picture_object = Picture.objects.get(link_info=picture_link)
             continue
-        except Painter.DoesNotExist:
+        except Picture.DoesNotExist:
             req = requests.get(picture_link, headers=headers, verify=False)
             soup = BeautifulSoup(req.content, 'html.parser')
 
             picture_name = str(soup.find('h1', class_='panel-title').contents[0])
+
+            file_url = BASE_URL + soup.find('a', class_='sui font-weight-400 font-size-16')['href']
+            filepath = 'static/pictures/' + uuid.uuid4().hex[:6].lower() + '.jpeg'
+            with open(filepath, "wb") as file:
+                new_headers = headers.copy()
+                new_headers['Referer'] = picture_link
+                new_headers['Sec-Fetch-Mode'] = 'navigate'
+                new_headers['Sec-Fetch-Site'] = 'same-origin'
+                response = requests.get(file_url, headers=new_headers, verify=False)
+                soup = BeautifulSoup(response.content, 'html.parser')
+
+                new_headers['Cookie'] = 'LOC=6%3ARU; _ym_uid=1584193274307759205; _ym_d=1584193274; _ga=GA1.2.1316766645.1584193275; marker=qoshppbs1u9cdu4pbkdhhav6p3; _fbp=fb.1.1584193335644.763158178; __gads=ID=1a20c5ae5e2af1b2:T=1584193335:S=ALNI_Mb_geQU23L3XbyDT6Z_Iu6IQdfUjQ; __utmz=63997296.1584193347.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); fid=6218156f-a46e-438f-a9c5-2de16cce6cc5; PHPSESSID=baf6949b24f2c14cc851cca346d422a6; _gid=GA1.2.67504545.1584389092; _ym_isad=2; _ym_visorc_253414=w; __utma=63997296.1316766645.1584193275.1584193347.1584475219.2; __utmc=63997296; __utmb=63997296.5.10.1584475219; _ym_wasSynced=%7B%22time%22%3A1584475551446%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; _ym_visorc_62733=w'
+                link = 'https:' + soup.find('a')['href']
+                response = requests.get(link, headers=new_headers, verify=False)
+
+                file.write(response.content)
+                file.close()
+
+            picture_object = Picture()
+            picture_object.name = picture_name
+            picture_object.painter = painter_object
+            picture_object.link_info = picture_link
+            picture_object.file = filepath
+            picture_object.save()
